343e7e7698819475671f77473411ca409e186397
ayorex11 <adewalemaxwell11@gmail.com>
2025-06-24 15:30:55 +0100

    transaction db

--- bank_app2.py
+++ bank_app2.py
@@ -6,6 +6,9 @@ import os
 import time
 
 DB_FILE = 'db.json'
+TRANSACTION_FILE = 'transaction.json'
+
+
 def load_data():
 	if not os.path.exists(DB_FILE):
 		return{}
@@ -22,12 +25,25 @@ def save_data(data):
 		json.dump(data, file, indent=4)
 
 
+def load_transaction_data():
+	if not os.path.exists(TRANSACTION_FILE):
+		return{}
 
+	try:
+		with open (TRANSACTION_FILE, 'r') as file:
+			return json.load(file)
+	except json.JSONDecodeError:
+		print ('Warning:  File corrupted or empty. starting fresh')
+		return {}
+
+def save_transaction_data(data):
+	with open(TRANSACTION_FILE, 'a') as file:
+		json.dump(data, file, indent=4)
 
 def hashpin(pin):
 	return hashlib.sha256(pin.encode()).hexdigest()
 
-def create_account(data):
+def create_account(data, data2):
 	print ('Welcome, we are happy to have you join us')
 	first_name = input('first name:')
 	last_name = input('last name:')
@@ -71,7 +87,10 @@ def create_account(data):
 			}
 
 	data[acc_num] = deets
+	data2[acc_num] = {}
 	save_data(data)
+	save_transaction_data(data2)
+
 	print(f'Welcome {first_name}, your account number is {acc_num}')
 	print('To login to your account, enter 1')
 	print('To close the app enter any other number')
@@ -89,9 +108,10 @@ def create_account(data):
 		return 
 
 
-def login(data):
+def login(data, data2):
     acc_num = input('Please enter your account number: ')
     user = data.get(acc_num)
+    transaction = data2.get(acc_num)
     
     if not user:
         print('No data found for account number entered')
@@ -101,7 +121,41 @@ def login(data):
     while pin_trials > 0:
         pin = input('Please enter your 4-digit PIN: ')
         if hashpin(pin) == user['pin']:
-            print (f'welcome once again {user['first_name']}, your account balance is {user['bal']}')
+            print (f'welcome once again {user['first_name']}, your account balance is #{user['bal']}')
+            while True:
+            	print("""\nTo check account balance, enter 1
+            	To deposit money, enter 2
+            	To transfer money, enter 3 
+            	To withdraw money, enter 4
+            	To logout, enter 5""")
+            	try:
+            		option = int(input('Enter an option \n >'))
+            	except ValueError:
+            		print('Invalid Input')
+
+            	if option == 1:
+            		pin = input('Please enter your pin to confirm \n >')
+            		if hashpin(pin) == user['pin']:
+            			print (f'Your account balance is #{user['bal']}')
+
+            	elif option == 2:
+            		try:
+            			amount = int(input('Please enter amount you wish to deposit \n >'))
+            		except ValueError:
+            			print('Invalid Input')
+            			return
+            		data[acc_num]['bal'] += amount
+            		save_data(data)
+            		time = str(datetime.now())
+
+            		deets = {'details': 'You money has been successfully deposited',
+            				'amount':amount,
+            				'Time' : time}
+
+            		data2[acc_num] = deets
+            		save_transaction_data(data2)
+            		print(f'You have successfully deposited #{amount} into your account')
+            
             
             return
         else:
@@ -116,14 +170,15 @@ def login(data):
 
 def interface():
 	data = load_data()
+	data2 = load_transaction_data()
 	print("Welcome to Wemma Bank")
 	print(" To create account enter 1\n To login enter 2 ")
 	option = int(input("Enter option >"))
 
 	if option == 1:
-		create_account(data)
+		create_account(data, data2)
 
 	if option == 2:
-		login(data)
+		login(data, data2)
 
 interface()
