35c0b8220f849d458e0c4c05b931495e0a54e9ce
ayorex11 <adewalemaxwell11@gmail.com>
2025-07-08 15:22:31 +0100

    transaction history

--- bank_app2.py
+++ bank_app2.py
@@ -2,239 +2,268 @@ from datetime import datetime, date
 import random
 import hashlib
 import json
-import os  
+import os
 import time
 
 DB_FILE = 'db.json'
 TRANSACTION_FILE = 'transaction.json'
 
-
 def load_data():
-	if not os.path.exists(DB_FILE):
-		return{}
+    if not os.path.exists(DB_FILE):
+        return {}
 
-	try:
-		with open (DB_FILE, 'r') as file:
-			return json.load(file)
-	except json.JSONDecodeError:
-		print ('Warning:  File corrupted or empty. starting fresh')
-		return {}
+    try:
+        with open(DB_FILE, 'r') as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        print('Warning: File corrupted or empty. starting fresh')
+        return {}
 
 def save_data(data):
-	with open(DB_FILE, 'w') as file:
-		json.dump(data, file, indent=4)
-
+    with open(DB_FILE, 'w') as file:
+        json.dump(data, file, indent=4)
 
 def load_transaction_data():
-	if not os.path.exists(TRANSACTION_FILE):
-		return{}
+    if not os.path.exists(TRANSACTION_FILE):
+        return {}
 
-	try:
-		with open (TRANSACTION_FILE, 'r') as file:
-			return json.load(file)
-	except json.JSONDecodeError:
-		print ('Warning:  File corrupted or empty. starting fresh')
-		return {}
+    try:
+        with open(TRANSACTION_FILE, 'r') as file:
+            return json.load(file)  # Expecting dict of lists
+    except json.JSONDecodeError:
+        print('Warning: File corrupted or empty. starting fresh')
+        return {}
 
 def save_transaction_data(data):
-	with open(TRANSACTION_FILE, 'a') as file:
-		json.dump(data, file, indent=4)
+    with open(TRANSACTION_FILE, 'w') as file:  # overwrite file, not append
+        json.dump(data, file, indent=4)
 
 def hashpin(pin):
-	return hashlib.sha256(pin.encode()).hexdigest()
+    return hashlib.sha256(pin.encode()).hexdigest()
 
 def create_account(data, data2):
-	print ('Welcome, we are happy to have you join us')
-	first_name = input('first name:')
-	last_name = input('last name:')
-	DOB_str = input('Date of Birth (input in this format YYYY/MM/DD): ')
-	DOB = datetime.strptime(DOB_str, '%Y/%m/%d').date()
+    print('Welcome, we are happy to have you join us')
+    first_name = input('first name:')
+    last_name = input('last name:')
+    DOB_str = input('Date of Birth (input in this format YYYY/MM/DD): ')
+    DOB = datetime.strptime(DOB_str, '%Y/%m/%d').date()
 
-	today = date.today()
-	age = today.year - DOB.year - ((today.month, today.day) < (DOB.month, DOB.day))
-	if age < 18:
-		print('You are not allowed to open an account')
-		return
+    today = date.today()
+    age = today.year - DOB.year - ((today.month, today.day) < (DOB.month, DOB.day))
+    if age < 18:
+        print('You are not allowed to open an account')
+        return
 
-	pin = input('input your desired pin:')
-	if not any(char.isdigit() for char in pin):
-		print('invalid input')
-		return
-	if len(pin)>4:
-		print('pin cannot be greater than 4')
-		return
-	if len(pin)<4:
-		print('pin cannot be lesser than 4')
-		return
+    pin = input('input your desired pin:')
+    if not any(char.isdigit() for char in pin):
+        print('invalid input')
+        return
+    if len(pin) > 4:
+        print('pin cannot be greater than 4')
+        return
+    if len(pin) < 4:
+        print('pin cannot be lesser than 4')
+        return
 
-	hashedpin = hashpin(pin)
-		
-	while True:
-		num = [1,2,3,4,5,6,7,8,9,0]
-		acc_num_list = ["5"]
-		acc_num_list.extend([str(random.choice(num)) for _ in range(9)])
-		acc_num = "".join(acc_num_list)
+    hashedpin = hashpin(pin)
 
-		if acc_num not in data:
-			break
+    while True:
+        num = [1,2,3,4,5,6,7,8,9,0]
+        acc_num_list = ["5"]
+        acc_num_list.extend([str(random.choice(num)) for _ in range(9)])
+        acc_num = "".join(acc_num_list)
 
+        if acc_num not in data:
+            break
 
-	deets = {'first_name': first_name,
-			  'last_name': last_name,
-			  'DOB': DOB_str,
-			  'pin': hashedpin,
-			  'bal': 0
-			}
+    deets = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'DOB': DOB_str,
+        'pin': hashedpin,
+        'bal': 0
+    }
 
-	data[acc_num] = deets
-	data2[acc_num] = {}
-	save_data(data)
-	save_transaction_data(data2)
+    data[acc_num] = deets
+    data2[acc_num] = []  # Initialize empty list for transactions
+    save_data(data)
+    save_transaction_data(data2)
 
-	print(f'Welcome {first_name}, your account number is {acc_num}')
-	print('To login to your account, enter 1')
-	print('To close the app enter any other number')
-	try:
-		option = int(input("Enter option >"))
-	except ValueError:
-		print('invalid input')
-		return
-	if option == 1:
-		login(data, data2)
-	else:
-		print ('logging out...')
-		time.sleep(3)
-		print('you have been successfully logged out')
-		return 
+    print(f'Welcome {first_name}, your account number is {acc_num}')
+    print('To login to your account, enter 1')
+    print('To close the app enter any other number')
+    try:
+        option = int(input("Enter option >"))
+    except ValueError:
+        print('invalid input')
+        return
+    if option == 1:
+        login(data, data2)
+    else:
+        print('logging out...')
+        time.sleep(3)
+        print('you have been successfully logged out')
+        return
 
+def add_transaction(data2, acc_num, deets):
+    if acc_num not in data2:
+        data2[acc_num] = []
+    data2[acc_num].append(deets)
+    save_transaction_data(data2)
 
 def login(data, data2):
     acc_num = input('Please enter your account number: ')
     user = data.get(acc_num)
-    transaction = data2.get(acc_num)
-    
+
     if not user:
         print('No data found for account number entered')
         return
 
-    pin_trials = 4 
+    pin_trials = 4
     while pin_trials > 0:
         pin = input('Please enter your 4-digit PIN: ')
         if hashpin(pin) == user['pin']:
-            print (f'welcome once again {user['first_name']}, your account balance is #{user['bal']}')
+            print(f"welcome once again {user['first_name']}, your account balance is #{user['bal']}")
             while True:
-            	print("""\nTo check account balance, enter 1
-            		To deposit money, enter 2
-            		To transfer money, enter 3 
-            		To withdraw money, enter 4
-            		To get transaction history, enter 5
-            		To logout, enter 0""")
-            	try:
-            		option = int(input('Enter an option \n >'))
-            	except ValueError:
-            		print('Invalid Input')
+                print("""\nTo check account balance, enter 1
+                    To deposit money, enter 2
+                    To transfer money, enter 3 
+                    To withdraw money, enter 4
+                    To get transaction history, enter 5
+                    To logout, enter 0""")
+                try:
+                    option = int(input('Enter an option \n >'))
+                except ValueError:
+                    print('Invalid Input')
+                    continue
 
-            	if option == 1:
-            		pin = input('Please enter your pin to confirm \n >')
-            		if hashpin(pin) == user['pin']:
-            			print (f'Your account balance is #{user['bal']}')
+                if option == 1:
+                    pin_confirm = input('Please enter your pin to confirm \n >')
+                    if hashpin(pin_confirm) == user['pin']:
+                        print(f"Your account balance is #{user['bal']}")
+                    else:
+                        print('Incorrect Pin')
 
-            		else:
-            			print('Incorrect Pin')
+                elif option == 2:
+                    try:
+                        amount = int(input('Please enter amount you wish to deposit \n >'))
+                    except ValueError:
+                        print('Invalid Input')
+                        continue
+                    data[acc_num]['bal'] += amount
+                    save_data(data)
+                    t = str(datetime.now())
 
-            	elif option == 2:
-            		try:
-            			amount = int(input('Please enter amount you wish to deposit \n >'))
-            		except ValueError:
-            			print('Invalid Input')
-            			return
-            		data[acc_num]['bal'] += amount
-            		save_data(data)
-            		time = str(datetime.now())
+                    deets = {
+                        'details': 'Your money has been successfully deposited',
+                        'amount': amount,
+                        'Time': t
+                    }
 
-            		deets = {'details': 'You money has been successfully deposited',
-            				'amount':amount,
-            				'Time' : time}
+                    add_transaction(data2, acc_num, deets)
+                    print(f'You have successfully deposited #{amount} into your account')
 
-            		data2[acc_num] = deets
-            		save_transaction_data(data2)
-            		print(f'You have successfully deposited #{amount} into your account')
+                elif option == 3:
+                    dest_account = input('Please enter destination account of user \n >')
+                    dest_user = data.get(dest_account)
+                    if not dest_user:
+                        print('Destination account not found')
+                        continue
+                    try:
+                        amount = int(input('Please enter amount you wish to send \n >'))
+                    except ValueError:
+                        print('Invalid Input')
+                        continue
+                    if amount > data[acc_num]['bal']:
+                        print('amount greater than current balance')
+                        continue
 
-            	elif option == 3:
-            		dest_account = input('Please enter destination account of user \n >')
-            		dest_user = data.get(dest_account)
-            		if not dest_user:
-            			print('Destination account not found')
-            			return
-            		amount = int(input('Please enter amount you wish to send \n >'))
-            		if amount > data[acc_num]['bal']:
-            			print('amount greater than current balance')
-            			return
+                    receiver = dest_user['first_name'] + " " + dest_user['last_name']
+                    print(f'You are about to send {amount} to {receiver}')
+                    trans_pin = input('Please enter your pin to confirm the transaction \n >')
+                    if hashpin(trans_pin) == user['pin']:
+                        data[acc_num]['bal'] -= amount
+                        data[dest_account]['bal'] += amount
+                        save_data(data)
 
-            		receiver = dest_user['first_name'] + " " + dest_user['last_name']
-            		print(f'You are about to send {amount} to {receiver}')
-            		trans_pin = input('Please enter your pin to confirm the transaction \n >')
-            		if hashpin(trans_pin) == user['pin']:
-            			data[acc_num]['bal'] -= amount
-            			data[dest_account]['bal'] += amount
-            			save_data(data)
+                        t = str(datetime.now())
 
-            			time = str(datetime.now())
-            			
-            			deets_sender = {
-            				'details' : 'You have successfully made a transfer',
-            				'amount' : amount,
-            				'Time' : time,
-            				'receiver' : receiver,
-            				'account_number' : dest_account
-            			}
+                        deets_sender = {
+                            'details': 'You have successfully made a transfer',
+                            'amount': amount,
+                            'Time': t,
+                            'receiver': receiver,
+                            'account_number': dest_account
+                        }
 
-            			data2[acc_num] = deets_sender
+                        add_transaction(data2, acc_num, deets_sender)
 
-            			sender = user['first_name'] + " " + user['last_name']
+                        sender = user['first_name'] + " " + user['last_name']
 
+                        deets_receiver = {
+                            'details': 'You have successfully received some money',
+                            'amount': amount,
+                            'Time': t,
+                            'sender': sender
+                        }
 
-            			deets_receiver = {
-            				'details' : 'You have successfully received some money',
-            				'amount' : amount,
-            				'Time' : time,
-            				'sender' : sender
-            			}
+                        add_transaction(data2, dest_account, deets_receiver)
+                        print('transfer successful')
 
-            			data2[dest_account] = deets_receiver
-            			save_transaction_data(data2)
-            			print('transfer successful')
+                    else:
+                        print('Invalid PIN')
 
+                elif option == 4:
+                    try:
+                        amount = int(input('Enter amount you wish to withdraw \n >'))
+                    except ValueError:
+                        print('Invalid Input')
+                        continue
 
-            		else:
-            			print('Invalid PIN')
+                    if amount > data[acc_num]['bal']:
+                        print('amount exceeds current balance')
+                        continue
 
-           		elif option == 4:
-           			try:
-           				amount = int(input('Enter amount you wish to withdraw \n >'))
-           			except ValueError:
-           				print('Invalid Input')
-           			if amount > data[acc_num]['bal']:
-           				print('amount exceeds current balance')
-           				return
+                    pin_confirm = input('Please enter your pin to confirm \n >')
+                    if hashpin(pin_confirm) == user['pin']:
+                        data[acc_num]['bal'] -= amount
+                        save_data(data)
+                        t = str(datetime.now())
+                        deets = {
+                            'details': 'Your withdrawal was successful',
+                            'amount': amount,
+                            'Time': t
+                        }
 
-           			pin = input('Please enter your pin to confirm \n >')
-            		if hashpin(pin) == user['pin']:
-            			data[acc_num]['bal'] -= amount
-            			save_data(data)
-            			time = str(datetime.now())
-            			deets = {'details': 'Your withdrawal was successful',
-            					'amount': amount,
-            					'Time': time}
+                        add_transaction(data2, acc_num, deets)
+                        print(f'you have successfully withdrawn #{amount}')
+                    else:
+                        print('Incorrect Pin')
 
-            			data2[acc_num] = deets
-            			save_transaction_data(data2)
-            			print (f'you have successfully withdrawn # {amount}')
+                elif option == 5:
+                    pin_confirm = input('Please enter your pin to view transaction \n >')
+                    if hashpin(pin_confirm) == user['pin']:
+                        transactions = data2.get(acc_num, [])
+                        if transactions:
+                            print('Below is all transactions done on this account:')
+                            for i, trans in enumerate(transactions, 1):
+                                print(f"Transaction {i}:")
+                                for key, value in trans.items():
+                                    print(f"  {key}: {value}")
+                                print()
+                        else:
+                            print('No transaction found for this account')
+                    else:
+                        print('Incorrect Pin')
 
-            		else:
-            			print('Incorrect Pin')
-            			
-            
+                elif option == 0:
+                    print('Logging out...')
+                    time.sleep(2)
+                    break
+
+                else:
+                    print('Invalid option. Please try again.')
+
             return
         else:
             pin_trials -= 1
@@ -244,23 +273,21 @@ def login(data, data2):
                 print('Too many failed attempts. Access denied.')
                 return
 
-
-
 def interface():
-	data = load_data()
-	data2 = load_transaction_data()
-	print("Welcome to Wemma Bank")
-	print(" To create account enter 1\n To login enter 2 ")
-	try:
-		option = int(input("Enter option >"))
-	except ValueError:
-		print('Invalid Input')
-		return
+    data = load_data()
+    data2 = load_transaction_data()
+    print("Welcome to Wemma Bank")
+    print(" To create account enter 1\n To login enter 2 ")
+    try:
+        option = int(input("Enter option >"))
+    except ValueError:
+        print('Invalid Input')
+        return
 
-	if option == 1:
-		create_account(data, data2)
+    if option == 1:
+        create_account(data, data2)
 
-	if option == 2:
-		login(data, data2)
+    if option == 2:
+        login(data, data2)
 
 interface()
